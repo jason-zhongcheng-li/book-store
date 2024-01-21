026fef2033b8de00e4ec8b4c054ba3b69f78404c
ZHONGCHENG LI <zhongcheng@192-168-1-102.tpgi.com.au>
2024-01-21 15:55:42 +1100

    feat: added graphql folder

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,50 +2,12 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { PrismaClient } from "@prisma/client";
 import prisma from "@/prisma/db";
+import { typeDefs } from "@/graphql/schema";
+import { resolvers } from "@/graphql/resolvers";
 
 export type Context = {
   prisma: PrismaClient;
 };
-
-const resolvers = {
-  Query: {
-    novels: async (parent: any, args: any, context: Context) =>
-      await context.prisma.novel.findMany(),
-    novel: async (parent: any, args: any, context: Context) =>
-      await context.prisma.novel.findUnique({ where: { id: args.novelId } }),
-  },
-
-  Novel: {
-    authors: async (parent: any, args: any, context: Context) =>
-      await context.prisma.author.findMany({ where: { novelId: parent.id } }),
-  },
-};
-
-const typeDefs = `#graphql
-  type Novel {
-    id: ID!
-    title: String
-    image: String
-    createdAt: String
-    updatedAt: String
-    authors: [Author]
-  }
-
-  type Author {
-    id: ID!
-    name: String
-    novelID: String
-  }
-
-  type Query {
-    novel(id: ID): Novel
-    novels: [Novel]
-  }
-
-  # type Mutation {
-  #   addNovel: Novel
-  # }
-`;
 
 const server = new ApolloServer<Context>({
   resolvers,
